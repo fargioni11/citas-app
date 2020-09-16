1445a874e2b66bba43781025cbee64aed1c7955c
unknown <adrielfargioni@gmail.com>
2020-09-16 18:05:26 -0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <div className="footer">
+            <p class="footer__title">Adriel Fargioni</p>
+            <div class="footer__social">
+                <a href="https://www.facebook.com/adriel.fargioni.7" class="footer__icon"><i class='bx bxl-facebook' ></i></a>
+                <a href="https://www.instagram.com/adrielfargiioni/" class="footer__icon"><i class='bx bxl-instagram' ></i></a>
+                <a href="https://twitter.com/Adriel61167998" class="footer__icon"><i class='bx bxl-twitter' ></i></a>
+            </div>
+            <p>&#169; 2020 copyright all right reserved</p>
+        </div>
+     );
+}
+ 
+export default Footer;
